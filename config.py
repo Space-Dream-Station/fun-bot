7cab8713b77170b5e940586f81cdef2acbaec1dc
Schrodinger71 <sanya.podshibyakin@mail.ru>
2026-01-22 15:32:15 +0300

    пупу

--- config.py
+++ config.py
@@ -27,6 +27,6 @@ def get_env_variable(name: str, default: str = "NULL") -> str:
 
 # Получение переменных из окружения
 DISCORD_KEY = get_env_variable("DISCORD_KEY")
-DISCORD_GUILD_ID = int(get_env_variable("DISCORD_GUILD_ID", "1354120935225167883"))
+DISCORD_GUILD_ID = 1354120935225167883
 DISCORD_TARGET_USER_ID = int(get_env_variable("DISCORD_TARGET_USER_ID", "1230142922314354840"))
-DISCORD_BAN_COOLDOWN = int(get_env_variable("DISCORD_BAN_COOLDOWN", "60"))
+DISCORD_BAN_COOLDOWN = 60
